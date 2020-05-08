84b815b8ff7be75fb925804268c8ab698d17ad48
Ghayrat Kholboev <ghayrat99@icloud.com>
2020-05-08 15:25:10 -0400

    add Cybertek.java

---
+++ src/day_1/Cybertek.java
@@ -0,0 +1,13 @@
+package day_1;
+
+public class Cybertek {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("The Best School In The World Cyberetek!!!");
+
+
+
+    }
+}
