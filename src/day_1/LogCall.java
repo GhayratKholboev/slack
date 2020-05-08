e5ef1cb011ce5d85b24aaeff854e600a66591207
GhayratKholboev <64181246+GhayratKholboev@users.noreply.github.com>
2020-05-08 15:32:29 -0400

    Update LogCall.java

--- src/day_1/LogCall.java
+++ src/day_1/LogCall.java
@@ -4,6 +4,9 @@ public class LogCall {
 
     public static void main(String[] args) {
         System.out.println("Cybertek ");
+        
+        System.out.println("Hello Cybertek");
+       
 
     }
 }
